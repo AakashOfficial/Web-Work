6d53651657b449ea3e327ae1ee5b1d9b38de8346
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-20 00:08:20 +0530

    Pattern Four !

--- Php/Pattern/Pattern4.php
+++ Php/Pattern/Pattern4.php
@@ -1,12 +1,8 @@
 <?php
-
-for ($row = 1; $row <= 5; $row++)
-{
- for ($col = 1; $col <= 5; $col++)
-  {
-   echo '* ';
-  }
-   echo "</br>";
+for($i=1;$i<=5;$i++) {
+	for($j=1;$j<=5;$j++) {
+		echo "*";
+	}
+	echo "<br>";
 }
-
 ?>
